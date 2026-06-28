367d1f86b868f233f72d9e86d66ae7a0fe6921f0
SANGEETH <43245950+sangeethkc@users.noreply.github.com>
2026-06-28 06:08:50 +0530

    Add allowedHosts to Vite preview configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    allowedHosts: ["app-006.notgr.xyz"],
+    // or to allow all hosts:
+    // allowedHosts: true,
+  },
   plugins: [react()],
 })
